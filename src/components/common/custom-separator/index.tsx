477cc58b89b2fab698f56130249b699e64e3e420
airkross <airkross123@gmail.com>
2023-01-05 00:06:47 +0300

    [learn-react] feat: add custom-separator component

---
+++ src/components/common/custom-separator/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import styles from './styles.module.css'
+
+const CustomSeparator: FC = () => {
+  return (
+   <hr className={styles.customSeparator} />
+  )
+}
+
+export default CustomSeparator
