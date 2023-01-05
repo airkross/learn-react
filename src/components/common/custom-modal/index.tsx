f8ec1f667a384c8e9e59d0e286c82b6b299f48cd
airkross <airkross123@gmail.com>
2023-01-05 20:24:06 +0300

    [learn-react] fix: fix custom-modal

--- src/components/common/custom-modal/index.tsx
+++ src/components/common/custom-modal/index.tsx
@@ -4,20 +4,21 @@ import styles from './styles.module.css'
 import { IProps } from './types'
 
 const CustomModal: FC<IProps> = ({ children, isShownModal, setIsShownModal }) => {
-  return (
-    <div className={[
-        styles.customModal,
-        isShownModal 
-            ? styles.active
-            : ''
-        ].join(' ')}
-        onClick={() => setIsShownModal(false)}    
-    >
-        <div className={styles.customModalContent} onClick={(e) => e.stopPropagation()}>
-            { children }
+    const classes = [styles.customModal]
+
+    if(isShownModal) {
+        classes.push( styles.active)
+    }
+
+    return (
+        <div className={classes.join(' ')}
+            onClick={() => setIsShownModal(false)}    
+        >
+            <div className={styles.customModalContent} onClick={(e) => e.stopPropagation()}>
+                { children }
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CustomModal
