bfe17860116148e078bcf0e7d202199ececbc8bc
airkross <airkross123@gmail.com>
2023-01-06 18:32:29 +0300

    [learn-react] feat: add custom-loader component

---
+++ src/components/common/custom-loader/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import styles from './styles.module.css'
+
+const CustomLoader: FC = () => {
+  return (
+    <div className={styles.customLoader}></div>
+  )
+}
+
+export default CustomLoader
