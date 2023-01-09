a3db3cb4624ac03e68c4c8f8e12c5f1d0a7152d5
airkross <airkross123@gmail.com>
2023-01-09 23:11:30 +0300

    [learn-react] feat: add home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+import styles from './styles.module.css'
+
+const HomePage: FC = () => {
+  return (
+    <div className={styles.homePage}>
+        <h1>Учу React</h1>
+    </div>
+  )
+}
+
+export default HomePage
