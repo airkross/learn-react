e87a735b9c4debc482458b3a2373b09eb139f809
airkross <airkross123@gmail.com>
2023-01-09 23:17:36 +0300

    [learn-react] fix: fixes

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -3,19 +3,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import NavigationMenu from './components/navigation-menu';
 import { routes } from './constants';
+import styles from './styles.module.css'
 
 
 const DefaultLayout: FC = () => {
     return (
         <BrowserRouter>
             <NavigationMenu />
-            <Routes>
-                {
-                    routes.map((route, index) => (
-                        <Route key={index} {...route} />
-                    ))
-                }
-            </Routes>
+            <div className={styles.content}>
+                <Routes>
+                    {
+                        routes.map((route, index) => (
+                            <Route key={index} {...route} />
+                        ))
+                    }
+                </Routes>
+            </div>
         </BrowserRouter>
     )
 }
