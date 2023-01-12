56a2140f5e76be96b6d5cf844d352dd1baacc80f
airkross <airkross123@gmail.com>
2023-01-12 23:19:40 +0300

    [learn-react] fix: refactoring

--- src/helpers/has.ts
+++
@@ -1,5 +0,0 @@
-type HasOwnPropertyCall = typeof Object.prototype.hasOwnProperty.call
-
-export function has(obj: Parameters<HasOwnPropertyCall>[0], key: Parameters<HasOwnPropertyCall>[1]): boolean {
-   return Object.prototype.hasOwnProperty.call(obj, key)
-}
